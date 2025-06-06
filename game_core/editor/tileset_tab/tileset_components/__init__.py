b7954077e675668dacbc28e3140efe861b40ebc9
BRENT¥ <82448930+brentPhil@users.noreply.github.com>
2025-06-06 13:19:07 +0800

    Add dungeon animated tileset

--- game_core/editor/tileset_tab/tileset_components/__init__.py
+++ game_core/editor/tileset_tab/tileset_components/__init__.py
@@ -3,5 +3,11 @@
 from .overworld_tileset import OverworldTileset
 from .overworld_anim_tileset import OverworldAnimTileset
 from .dungeon_tileset import DungeonTileset
+from .dungeon_anim_tileset import DungeonAnimTileset
 
-__all__ = ["OverworldTileset", "OverworldAnimTileset", "DungeonTileset"]
+__all__ = [
+    "OverworldTileset",
+    "OverworldAnimTileset",
+    "DungeonTileset",
+    "DungeonAnimTileset",
+]
