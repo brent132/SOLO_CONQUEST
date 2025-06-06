d640bc861ea77ad43b44f8d3eca35d664e71e46a
BRENT¥ <82448930+brentPhil@users.noreply.github.com>
2025-06-06 16:29:03 +0800

    Add canvas controls for panning, zooming and dragging

--- game_core/editor/canvas/__init__.py
+++ game_core/editor/canvas/__init__.py
@@ -3,5 +3,11 @@
 from .canvas import Canvas
 from .tile_placement import TilePlacementManager
 from .tileset_repository import TilesetRepository
+from .canvas_controls import CanvasControls
 
-__all__ = ["Canvas", "TilePlacementManager", "TilesetRepository"]
+__all__ = [
+    "Canvas",
+    "TilePlacementManager",
+    "TilesetRepository",
+    "CanvasControls",
+]
