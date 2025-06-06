08a9196d71fb06aa1adc96d5ef5845f496f07ef8
BRENT¥ <82448930+brentPhil@users.noreply.github.com>
2025-06-06 14:28:47 +0800

    Add image cache to editor and update imports

--- game_core/editor/__init__.py
+++ game_core/editor/__init__.py
@@ -1,8 +1,9 @@
 """Editor package containing editor-specific modules."""
 
-__all__ = ["Sidebar", "Canvas", "TabManager", "TilesetTabManager"]
+__all__ = ["Sidebar", "Canvas", "TabManager", "TilesetTabManager", "sprite_cache"]
 
 from .sidebar import Sidebar
 from .canvas import Canvas
 from .sidebar_tab_manager import TabManager
 from .tileset_tab.tileset_tab_manager import TilesetTabManager
+from .image_cache import sprite_cache
